bc280f513724a6be2609ed9fdb04cbdc181e33a4
wuyd <wuydit@163.com>
2021-03-05 18:42:51 +0800

    feat(all): 启动类

--- dr-server/src/main/java/com/bear/server/DrServerApplication.java
+++ dr-server/src/main/java/com/bear/server/DrServerApplication.java
@@ -1,16 +1,25 @@
 package com.bear.server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author wuyd
  */
+@Slf4j
+@EnableAsync
 @SpringBootApplication
 public class DrServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DrServerApplication.class, args);
+		log.info("HTTP server start!");
+		ApplicationContext context = SpringApplication.run(DrServerApplication.class, args);
+		Environment env = context.getBean(Environment.class);
+		log.info("Server path: http://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
---
+++ dr-server/src/main/java/com/bear/server/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.bear.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author wuyd
+ * 2021/2/25 14:36
+ */
+
+@Configuration
+public class CorsConfig {
+
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
